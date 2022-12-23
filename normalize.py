3b37a4a8730336837a9329b4a742b6d380a05a2f
Timur <timurram007@gmail.com>
2022-12-23 13:40:46 +0300

    fix

--- normalize.py
+++ normalize.py
@@ -26,9 +26,15 @@ def normalize_numbers():
         for file in files:
             img_dir = f'{files_dir}/{file}'
             with Image.open(img_dir) as img:
-                img = img.crop((30, 30, img.height - 30, img.width - 30))
-                img.thumbnail((28, 28))
-                img.save(img_dir)
+                # img = img.crop((30, 30, img.height - 30, img.width - 30))
+                # img.thumbnail((28, 28))
+                # img.save(img_dir)
+                arr_caption=['2-2','-22','-2-2','22']
+                arr_translation = [(2, 2), (2, -2), (-2, 2), (-2, -2)]
+                for i in range(4):
+                    shift_img = Image.new("RGB", (28, 28), (255, 255, 255))
+                    shift_img.paste(img, box=arr_translation[i])
+                    shift_img.save(f'datasets/{n}/{arr_caption[i]}_{file}')
 
 
 def normalize_letters():
@@ -39,11 +45,20 @@ def normalize_letters():
         for file in files:
             img_dir = f'{files_dir}/{file}'
             with Image.open(img_dir) as img:
-                new_img = Image.new('RGBA', img.size, 'WHITE')
-                new_img.paste(img, (0, 0), img)
-                new_img.convert('RGB')
-                new_img.thumbnail((28, 28))
-                new_img.save(img_dir)
+                # new_img = Image.new('RGBA', img.size, 'WHITE')
+                # new_img.paste(img, (0, 0), img)
+                # new_img.convert('RGB')
+                # new_img.thumbnail((28, 28))
+                # new_img.save(img_dir)
+                # arr_caption=['2-2','-22','-2-2','22']
+                # arr_translation = [(2, 2), (2, -2), (-2, 2), (-2, -2)]
+                # for i in range(4):
+                #     shift_img = Image.new("RGB", (28, 28), (255, 255, 255))
+                #     shift_img.paste(new_img, box=arr_translation[i])
+                #     shift_img.save(f'datasets/{l}/{arr_caption[i]}_{file}')
+                shift_img = Image.new("RGB", (28, 28), (255, 255, 255))
+                shift_img.paste(img, box=(1, 0))
+                shift_img.save(f'datasets/{l}/1_{file}')
 
 
 def generate_y():
@@ -63,11 +78,13 @@ def generate_x():
             img = cv2.imread(f'./datasets/{ch}/{file}', 0)
             img = cv2.bitwise_not(img)
             img = img / 255.0
+            img = np.reshape(img, (28 * 28))
             arr = np.array(img)
             x.append(arr)
     x = np.array(x)
     pd.DataFrame(x).to_csv('x.csv')
 
 
-for i in alnums:
-    print(i, len(os.listdir(f'./datasets/{i}')))
+generate_x()
+print('generating y')
+generate_y()
