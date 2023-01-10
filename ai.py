b001fee0968f4bfe28a7ee117691e893d448311a
Timur <timurram007@gmail.com>
2023-01-10 11:09:30 +0300

    refactor

--- ai.py
+++ ai.py
@@ -22,148 +22,126 @@ extra = ',-'
 chrs = {'l': letters, 'n': numbers}
 
 
-def ege_model_1(type: str):
-    ''' Создает модель (тип 1) для распознования символов ЕГЭ'''
-    model = Sequential()
-    model.add(Convolution2D(filters=32, kernel_size=(3, 3), padding='valid', input_shape=(28, 28, 1), activation='relu'))
-    model.add(Convolution2D(filters=64, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(len(chrs[type]), activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
-    return model
+class EgeModel:
+
+    def __init__(self, type: str) -> None:
+        self.type = type
+        self.load()
+
+    def load(self) -> None:
+        if os.path.exists(f'keras_models/{self.type}_ege_model.h5'):
+            self.model = keras.models.load_model(f'keras_models/{self.type}_ege_model.h5')
+            print('Модель загружена из файла')
+        else:
+            print('Файл для загрузки не найден')
+            self.train()
+
+    def make_model(self) -> None:
+        ''' Создает модель (тип 1) для распознования символов ЕГЭ'''
+        self.model = Sequential()
+        self.model.add(Convolution2D(filters=32, kernel_size=(3, 3), padding='same', input_shape=(28, 28, 1), activation='relu'))
+        self.model.add(Convolution2D(filters=32, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
+        self.model.add(Convolution2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.model.add(Convolution2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        self.model.add(Dropout(0.25))
+        self.model.add(Flatten())
+        self.model.add(Dense(512, activation="relu"))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(len(chrs[self.type]), activation="softmax"))
+        self.model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0), metrics=['accuracy'])
 
 
-def ege_model_2(type: str):
-    ''' Создает модель (тип 1) для распознования символов ЕГЭ'''
-    model = Sequential()
-    model.add(Convolution2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same', input_shape=(28, 28, 1)))
-    model.add(MaxPooling2D((2, 2)))
-    model.add(Convolution2D(64, (3, 3), activation='relu', padding='same'))
-    model.add(MaxPooling2D((2, 2)))
-    model.add(Convolution2D(128, (3, 3), activation='relu', padding='same'))
-    model.add(MaxPooling2D((2, 2)))
-    model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(len(chrs[type]), activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
-    return model
+    def train(self):
+        ''' Обучает модель для символов ЕГЭ '''
+        print('Старт предобработки данных и обучения')
+        t_start = time.time() # время старта
+
+        x = pd.read_csv(f'./csv/{self.type}x.csv') # чтение csv с данными для обучения
+        x = x.drop(x.columns[[0]], axis=1) # удаление строки index
+        x = x.values # чтение массива с данными
+        y = pd.read_csv(f'./csv/{self.type}y.csv')['0'].values # чтение меток для обучения 
+
+        X_train, X_test, y_train, y_test = train_test_split(x, y, shuffle=True, test_size=0.15) # разделение данных на train и test
+
+        X_train = np.reshape(X_train, (X_train.shape[0], 28, 28, 1)) # "выпрямление" данных
+        X_test = np.reshape(X_test, (X_test.shape[0], 28, 28, 1))
+
+        print(X_train.shape, y_train.shape, X_test.shape, y_test.shape, len(chrs[self.type]))
+
+        y_train_cat = keras.utils.to_categorical(y_train, len(chrs[self.type])) # преобазование значения символа в форму выходного слоя нейросети
+        y_test_cat = keras.utils.to_categorical(y_test, len(chrs[self.type]))
+
+        learning_rate_reduction = keras.callbacks.ReduceLROnPlateau(monitor='val_accuracy', patience=3, verbose=1, factor=0.5, min_lr=0.00001)
+
+        self.model.fit(X_train, y_train_cat, validation_data=(X_test, y_test_cat), callbacks=[learning_rate_reduction], batch_size=128, epochs=30) # обучение нейросети
+        print(f"{self.type} Модель обучилась, dT (min):", (time.time() - t_start) / 60) # отчет о завершении и затраченное время в мин
+        self.model.save(f'keras_models/{self.type}_ege_model3.h5')
 
 
-def ege_model_3(type: str):
-    ''' Создает модель (тип 1) для распознования символов ЕГЭ'''
-    model = Sequential()
-    model.add(Convolution2D(filters=32, kernel_size=(3, 3), padding='same', input_shape=(28, 28, 1), activation='relu'))
-    model.add(Convolution2D(filters=32, kernel_size=(3, 3), padding='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Convolution2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
-    model.add(Convolution2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(512, activation="relu"))
-    model.add(Dropout(0.5))
-    model.add(Dense(len(chrs[type]), activation="softmax"))
-    model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0), metrics=['accuracy'])
-    return model
+    def ege_predict_img(self, img):
+        ''' Возвращает 1 символ на фото '''
+        img_arr = np.expand_dims(img, axis=0) 
+        img_arr = 1 - img_arr / 255.0 # конвертация диапазона 0-255 в 0-1
+        img_arr = img_arr.reshape((1, 28, 28, 1)) # "выпрямление" данных
+        predict = self.model.predict([img_arr]) # вычисление результата
+        result = np.argmax(predict, axis=1) # поиск символа в алфавите
+        return chrs[self.type][result[0]]
 
 
-def ege_train(model: Any, type: str):
-    ''' Обучает модель для символов ЕГЭ '''
-    print('Старт предобработки данных и обучения')
-    t_start = time.time() # время старта
+    def letters_extract(self, image_file: str, out_size=28):
+        ''' Выделяет и возвращает массив с символами на фото '''
+        img = cv2.imread(image_file) # чтение фото
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # обесцвечивание 
+        _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY) # повыение контраста
+        img_erode = cv2.erode(thresh, np.ones((3, 3), np.uint8), iterations=1) # утолшение символа
 
-    x = pd.read_csv(f'./csv/{type}x.csv') # чтение csv с данными для обучения
-    x = x.drop(x.columns[[0]], axis=1) # удаление строки index
-    x = x.values # чтение массива с данными
-    y = pd.read_csv(f'./csv/{type}y.csv')['0'].values # чтение меток для обучения 
+        contours, hierarchy = cv2.findContours(img_erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) # нахождение контуров символов
 
-    X_train, X_test, y_train, y_test = train_test_split(x, y, shuffle=True, test_size=0.15) # разделение данных на train и test
+        output = img.copy()
 
-    X_train = np.reshape(X_train, (X_train.shape[0], 28, 28, 1)) # "выпрямление" данных
-    X_test = np.reshape(X_test, (X_test.shape[0], 28, 28, 1))
+        letters = []
+        for idx, contour in enumerate(contours):
+            (x, y, w, h) = cv2.boundingRect(contour)
+            if hierarchy[0][idx][3] == 0:
+                cv2.rectangle(output, (x, y), (x + w, y + h), (70, 0, 0), 1)
+                letter_crop = gray[y:y + h, x:x + w]
+                size_max = max(w, h)
+                letter_square = 255 * np.ones(shape=[size_max, size_max], dtype=np.uint8)
+                if w > h:
+                    y_pos = size_max // 2 - h // 2
+                    letter_square[y_pos:y_pos + h, 0:w] = letter_crop
+                elif w < h:
+                    x_pos = size_max // 2 - w // 2
+                    letter_square[0:h, x_pos:x_pos + w] = letter_crop
+                else:
+                    letter_square = letter_crop
 
-    print(X_train.shape, y_train.shape, X_test.shape, y_test.shape, len(chrs[type]))
-
-    y_train_cat = keras.utils.to_categorical(y_train, len(chrs[type])) # преобазование значения символа в форму выходного слоя нейросети
-    y_test_cat = keras.utils.to_categorical(y_test, len(chrs[type]))
-
-    learning_rate_reduction = keras.callbacks.ReduceLROnPlateau(monitor='val_accuracy', patience=3, verbose=1, factor=0.5, min_lr=0.00001)
-
-    model.fit(X_train, y_train_cat, validation_data=(X_test, y_test_cat), callbacks=[learning_rate_reduction], batch_size=128, epochs=30) # обучение нейросети
-    print(f"{type} Модель обучилась, dT (min):", (time.time() - t_start) / 60) # отчет о завершении и затраченное время в мин
-
-
-def ege_predict_img(model, img, type: str):
-    ''' Возвращает 1 символ на фото '''
-    img_arr = np.expand_dims(img, axis=0) 
-    img_arr = 1 - img_arr / 255.0 # конвертация диапазона 0-255 в 0-1
-    img_arr = img_arr.reshape((1, 28, 28, 1)) # "выпрямление" данных
-    predict = model.predict([img_arr]) # вычисление результата
-    result = np.argmax(predict, axis=1) # поиск символа в алфавите
-    print(predict)
-    print(max(predict[0]))
-    return chrs[type][result[0]]
+                letters.append((x, w, cv2.resize(letter_square, (out_size, out_size), interpolation=cv2.INTER_AREA)))
 
 
-def letters_extract(image_file: str, out_size=28):
-    ''' Выделяет и возвращает массив с символами на фото '''
-    img = cv2.imread(image_file) # чтение фото
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # обесцвечивание 
-    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY) # повыение контраста
-    img_erode = cv2.erode(thresh, np.ones((3, 3), np.uint8), iterations=1) # утолшение символа
-
-    contours, hierarchy = cv2.findContours(img_erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) # нахождение контуров символов
-
-    output = img.copy()
-
-    letters = []
-    for idx, contour in enumerate(contours):
-        (x, y, w, h) = cv2.boundingRect(contour)
-        if hierarchy[0][idx][3] == 0:
-            cv2.rectangle(output, (x, y), (x + w, y + h), (70, 0, 0), 1)
-            letter_crop = gray[y:y + h, x:x + w]
-            size_max = max(w, h)
-            letter_square = 255 * np.ones(shape=[size_max, size_max], dtype=np.uint8)
-            if w > h:
-                y_pos = size_max // 2 - h // 2
-                letter_square[y_pos:y_pos + h, 0:w] = letter_crop
-            elif w < h:
-                x_pos = size_max // 2 - w // 2
-                letter_square[0:h, x_pos:x_pos + w] = letter_crop
-            else:
-                letter_square = letter_crop
-
-            letters.append((x, w, cv2.resize(letter_square, (out_size, out_size), interpolation=cv2.INTER_AREA)))
+        letters.sort(key=lambda x: x[0], reverse=False) # сортировка букв по Х координате
+        cv2.imshow("Letters", output)
+        cv2.waitKey(0)    
+        return letters
 
 
-    letters.sort(key=lambda x: x[0], reverse=False) # сортировка букв по Х координате
-    cv2.imshow("Letters", output)
-    cv2.waitKey(0)    
-    return letters
-
-
-def img_to_str(model: Any, image_file: str, type: str):
-    ''' Конвертирует символы на фотографии в строку '''
-    letters = letters_extract(image_file) # получение массива букв с фото
-    s_out = ""
-    for i in range(len(letters)):
-        s_out += ege_predict_img(model, letters[i][2], type) # присоединение следующей буквы к итоговой строке
-    return s_out
+    def img_to_str(self, image_file: str):
+        ''' Конвертирует символы на фотографии в строку '''
+        letters = self.letters_extract(image_file=image_file) # получение массива букв с фото
+        s_out = ""
+        for i in range(len(letters)):
+            s_out += self.ege_predict_img(letters[i][2]) # присоединение следующей буквы к итоговой строке
+        return s_out
 
 
 if __name__ == "__main__":
-    type = 'l'
+    # type = 'l'
     # model = ege_model_3(type)
     # emnist_train(model, type)
     # model.save(f'keras_models/{type}_ege_model3.h5')
-
-    model = keras.models.load_model(f'keras_models/{type}_ege_model3.h5')
-    print('Model loaded')
-    s_out = img_to_str(model, "photos/test.png", type)
+    ai = EgeModel('n')
+    s_out = ai.img_to_str("photos/test.png")
     print(s_out)
